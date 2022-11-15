8e65c6536b775fcdbfe65af5067bdce44602a630
Kevin <kevin005005@gmail.com>
2022-11-15 01:13:25 +0100

    Song downloader updated

--- Download songs.py
+++ Download songs.py
@@ -3,5 +3,7 @@ import os
 # Specify YT-DLP location here
 YT_DLP_LOCATION = "D:\Programs\yt-dlp\yt-dlp.exe"
 
-for link in open('list.txt').read().splitlines():
+ids = open('list.txt').read().splitlines()
+for i, link in enumerate(ids, 1):
+    print(f"{len(ids)}/{i} ({i/len(ids)*100:.2f}%)")
     os.system(f"{YT_DLP_LOCATION} -o \"Downloaded songs/%(title)s - %(channel)s.%(ext)s\" -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 {link}")
