87ac1b9f59fa01cdc94d2d703ce473e2fc092f4d
Kevin <kevin005005@gmail.com>
2022-11-15 02:35:41 +0100

    Updated YT_DLP location and link to the videos

--- Download songs.py
+++ Download songs.py
@@ -1,9 +1,9 @@
 import os
 
 # Specify YT-DLP location here
-YT_DLP_LOCATION = "D:\Programs\yt-dlp\yt-dlp.exe"
+YT_DLP_LOCATION = "D:\\Programs\\yt-dlp\\yt-dlp.exe"
 
 ids = open('list.txt').read().splitlines()
 for i, link in enumerate(ids, 1):
     print(f"{len(ids)}/{i} ({i/len(ids)*100:.2f}%)")
-    os.system(f"{YT_DLP_LOCATION} -o \"Downloaded songs/%(title)s - %(channel)s.%(ext)s\" -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 {link}")
+    os.system(f"{YT_DLP_LOCATION} -o \"Downloaded songs/%(title)s - %(channel)s.%(ext)s\" -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 https://www.youtube.com/watch?v={link}")
