425f73396fad88c4296622cf9415ce6fa61ccae0
Kevin <kevin005005@gmail.com>
2022-11-15 17:48:12 +0100

    PATH check included

--- Download songs.py
+++ Download songs.py
@@ -1,7 +1,10 @@
 import os
+from shutil import which
 
-# Specify YT-DLP location here
-YT_DLP_LOCATION = "D:\\Programs\\yt-dlp\\yt-dlp.exe"
+# Specify YT-DLP location here if it's not added to the PATH
+YT_DLP_LOCATION = "yt-dlp"
+if which("yt-dlp") is None:
+    YT_DLP_LOCATION = "D:\\Programs\\yt-dlp\\yt-dlp.exe"
 
 ids = open('list.txt').read().splitlines()
 for i, link in enumerate(ids, 1):
