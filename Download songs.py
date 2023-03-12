3eee02c3314f552197f5fd184634c29c227d1071
Kevin <kevin005005@gmail.com>
2023-03-12 17:39:32 +0100

    Added more options to the config

--- Download songs.py
+++ Download songs.py
@@ -8,16 +8,16 @@ video_lister = 'VideoLister'
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-emoji_stripper = '--replace-in-metadata title "[\U00010000-\U0010ffff]" ""'
-
 # Specify yt-dlp location here if it's not added to the PATH
 YT_DLP_LOCATION = 'yt-dlp'
 if which(YT_DLP_LOCATION) is None:
     YT_DLP_LOCATION = config[app_name]['yt_dlp_path']
 
 ids = open(config[video_lister]['list_path']).read().splitlines()
+if int(config[app_name]['start_from_oldest']):
+    ids = ids[::-1]
 for i, link in enumerate(ids, 1):
     print(f"{len(ids)}/{i} ({i / len(ids) * 100:.2f}%)")
     cmd = f"{YT_DLP_LOCATION} -o \"{config[app_name]['download_path']}/{config[app_name]['title_format']}\" " \
-          f"{config[app_name]['yt_dl_options']} {emoji_stripper} https://www.youtube.com/watch?v={link}"
+          f"{config[app_name]['video_format']} {config[app_name]['metadata_options']} https://www.youtube.com/watch?v={link}"
     os.system(cmd)
